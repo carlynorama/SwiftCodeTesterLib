0f6790a1ed56bc1c15a3e0bb22b1c4bdc1076b30
carlynorama <carlynorama@gmail.com>
2024-01-13 07:55:14 -0800

    test plugin

--- Plugins/MakeCodeSketch/plugin.swift
+++ Plugins/MakeCodeSketch/plugin.swift
@@ -4,7 +4,10 @@ import PackagePlugin
 struct MakeCodeSketch: CommandPlugin {
     func performCommand(context: PackagePlugin.PluginContext, 
                         arguments: [String]) async throws {
-        <#code#>
+        var message = "do I see this?"
+        print("do I see this?\nhow about now?")
+        let contributors = Set(message.components(separatedBy: CharacterSet.newlines)).sorted().filter { !$0.isEmpty }
+        try contributors.joined(separator: "\n").write(toFile: "CONTRIBUTORS.txt", atomically: true, encoding: .utf8)
     }
     
     
