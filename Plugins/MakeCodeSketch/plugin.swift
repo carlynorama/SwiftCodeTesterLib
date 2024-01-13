b9a5f5ff370dc6ffe168c6f7f8df5b40cffaa120
carlynorama <carlynorama@gmail.com>
2024-01-13 10:07:39 -0800

    log to the file

--- Plugins/MakeCodeSketch/plugin.swift
+++ Plugins/MakeCodeSketch/plugin.swift
@@ -5,12 +5,12 @@ import Foundation
 struct MakeCodeSketch: CommandPlugin {
     func performCommand(context: PackagePlugin.PluginContext, 
                         arguments: [String]) async throws {
-        let message = "do I see this?\nhow about now?"
+        var message = "do I see this?\nhow about now?"
         let fileName = "codeSketch" + ".txt"
         //let location = context.pluginWorkDirectory // Is the BUILD folder
-        var testLocation = context
+        message.append("\n\(context)\n\(arguments)")
         var location = context.package.directory.appending([fileName])
-        print(message, "\n" ,testLocation)
+        //print(message, "\n")
         let contributors = Set(message.components(separatedBy: CharacterSet.newlines)).sorted().filter { !$0.isEmpty }
         try contributors.joined(separator: "\n").write(toFile: location.string, atomically: true, encoding: .utf8)
     }
