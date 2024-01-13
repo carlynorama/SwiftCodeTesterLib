7d7ed84630e31810e576bfa962ee404bcfc619f8
carlynorama <carlynorama@gmail.com>
2024-01-13 09:28:31 -0800

    update package

--- Plugins/MakeCodeSketch/plugin.swift
+++ Plugins/MakeCodeSketch/plugin.swift
@@ -8,8 +8,9 @@ struct MakeCodeSketch: CommandPlugin {
         let message = "do I see this?\nhow about now?"
         let fileName = "codeSketch" + ".txt"
         //let location = context.pluginWorkDirectory // Is the BUILD folder
+        var testLocation = context
         var location = context.package.directory.appending([fileName])
-        print(message, "\n" ,location)
+        print(message, "\n" ,testLocation)
         let contributors = Set(message.components(separatedBy: CharacterSet.newlines)).sorted().filter { !$0.isEmpty }
         try contributors.joined(separator: "\n").write(toFile: location.string, atomically: true, encoding: .utf8)
     }
