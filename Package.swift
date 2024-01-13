cdea2a909ba51fb749abe064b6243eaad8c5cb57
carlynorama <carlynorama@gmail.com>
2024-01-13 09:21:51 -0800

    new package

--- Package.swift
+++ Package.swift
@@ -24,3 +24,20 @@ let package = Package(
         )
     ]
 )
+
+/*
+ Package(
+ id: "swiftcodetesterlib",
+ displayName: "SwiftCodeTesterLib",
+ directory: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib,
+ origin: PackagePlugin.PackageOrigin.root,
+ toolsVersion: PackagePlugin.ToolsVersion(major: 5, minor: 9, patch: 0),
+ dependencies: [],
+ products: [
+ PackagePlugin.LibraryProduct(id: "0", name: "CodeSketch", targets: [PackagePlugin.SwiftSourceModuleTarget(id: "0", name: "CodeSketch", kind: PackagePlugin.ModuleKind.generic, directory: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch, dependencies: [], moduleName: "CodeSketch", sourceFiles: PackagePlugin.FileList(files: [PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/CodeSketch.swift, type: PackagePlugin.FileType.source), PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/CodeSketchError.swift, type: PackagePlugin.FileType.source), PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/ExampleSketch.swift, type: PackagePlugin.FileType.source)]), compilationConditions: [], linkedLibraries: [], linkedFrameworks: [])], kind: PackagePlugin.LibraryProduct.Kind.automatic)
+ ],
+ targets: [
+ PackagePlugin.SwiftSourceModuleTarget(id: "0", name: "CodeSketch", kind: PackagePlugin.ModuleKind.generic, directory: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch, dependencies: [], moduleName: "CodeSketch", sourceFiles: PackagePlugin.FileList(files: [PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/CodeSketch.swift, type: PackagePlugin.FileType.source), PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/CodeSketchError.swift, type: PackagePlugin.FileType.source), PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/ExampleSketch.swift, type: PackagePlugin.FileType.source)]), compilationConditions: [], linkedLibraries: [], linkedFrameworks: [])
+ ]
+ )
+ */
--- Plugins/MakeCodeSketch/plugin.swift
+++ Plugins/MakeCodeSketch/plugin.swift
@@ -6,9 +6,12 @@ struct MakeCodeSketch: CommandPlugin {
     func performCommand(context: PackagePlugin.PluginContext, 
                         arguments: [String]) async throws {
         let message = "do I see this?\nhow about now?"
-        print(message)
+        let fileName = "codeSketch" + ".txt"
+        //let location = context.pluginWorkDirectory // Is the BUILD folder
+        var location = context.package.directory.appending([fileName])
+        print(message, "\n" ,location)
         let contributors = Set(message.components(separatedBy: CharacterSet.newlines)).sorted().filter { !$0.isEmpty }
-        try contributors.joined(separator: "\n").write(toFile: "CONTRIBUTORS.txt", atomically: true, encoding: .utf8)
+        try contributors.joined(separator: "\n").write(toFile: location.string, atomically: true, encoding: .utf8)
     }
     
     
