12c51eea0e222a7919635ae2c9ccd21c935b669a
carlynorama <carlynorama@gmail.com>
2024-01-14 14:25:20 -0800

    Add new plugin just for informational purposes

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     platforms: [.macOS(.v13)],
     products: [
         .library(name: "CodeSketch", targets: ["CodeSketch"]),
-        .plugin(name: "MakeCodeSketch", targets: ["MakeCodeSketch"])
+        .plugin(name: "MakeCodeSketch", targets: ["MakeCodeSketch"]),
+        .plugin(name: "TellMeAboutYourself", targets: ["TellMeAboutYourself"])
     ],
     targets: [
         .target(
@@ -21,23 +22,12 @@ let package = Package(
             capability: .command(intent: .custom(verb: "make-sketch", 
                                                  description: "Create an new code sketch"),
                                  permissions: [.writeToPackageDirectory(reason: "This plugin creates a new code sketch file.")])
+        ),
+        .plugin(
+            name: "TellMeAboutYourself",
+            capability: .command(intent: .custom(verb: "about",
+                                                 description: "See info about the package"),
+                                 permissions: [.writeToPackageDirectory(reason: "This plugin creates a file with information about the plugin and the package it's running on.")])
         )
     ]
 )
-
-/*
- Package(
- id: "swiftcodetesterlib",
- displayName: "SwiftCodeTesterLib",
- directory: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib,
- origin: PackagePlugin.PackageOrigin.root,
- toolsVersion: PackagePlugin.ToolsVersion(major: 5, minor: 9, patch: 0),
- dependencies: [],
- products: [
- PackagePlugin.LibraryProduct(id: "0", name: "CodeSketch", targets: [PackagePlugin.SwiftSourceModuleTarget(id: "0", name: "CodeSketch", kind: PackagePlugin.ModuleKind.generic, directory: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch, dependencies: [], moduleName: "CodeSketch", sourceFiles: PackagePlugin.FileList(files: [PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/CodeSketch.swift, type: PackagePlugin.FileType.source), PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/CodeSketchError.swift, type: PackagePlugin.FileType.source), PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/ExampleSketch.swift, type: PackagePlugin.FileType.source)]), compilationConditions: [], linkedLibraries: [], linkedFrameworks: [])], kind: PackagePlugin.LibraryProduct.Kind.automatic)
- ],
- targets: [
- PackagePlugin.SwiftSourceModuleTarget(id: "0", name: "CodeSketch", kind: PackagePlugin.ModuleKind.generic, directory: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch, dependencies: [], moduleName: "CodeSketch", sourceFiles: PackagePlugin.FileList(files: [PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/CodeSketch.swift, type: PackagePlugin.FileType.source), PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/CodeSketchError.swift, type: PackagePlugin.FileType.source), PackagePlugin.File(path: /Users/Carlyn/Developer/GitHub/SwiftNotes2024/SwiftCodeTesterLib/Sources/CodeSketch/ExampleSketch.swift, type: PackagePlugin.FileType.source)]), compilationConditions: [], linkedLibraries: [], linkedFrameworks: [])
- ]
- )
- */
--- Plugins/MakeCodeSketch/plugin.swift
+++ Plugins/MakeCodeSketch/plugin.swift
@@ -1,27 +1,76 @@
 import PackagePlugin
 import Foundation
 
+
+//TODO: Link to ArgumentParser compiled tool?
+//https://github.com/apple/swift-package-manager/blob/main/Documentation/Plugins.md#implementing-the-command-plugin-script
+
+
 @main
 struct MakeCodeSketch: CommandPlugin {
     func performCommand(context: PackagePlugin.PluginContext, 
                         arguments: [String]) async throws {
-        var message = "do I see this?\nhow about now?"
-        let fileName = "codeSketch" + ".txt"
-        //let location = context.pluginWorkDirectory // Is the BUILD folder
+        var argExtractor = ArgumentExtractor(arguments)
+        let targetNames = argExtractor.extractOption(named: "target")
+        let sketchNames = argExtractor.extractOption(named: "named")
         
-        message.append("\norigin: \(context.package.origin)")
-        message.append("\ndirectory: \(context.package.directory)")
-        message.append("\nsourceModules: \(context.package.sourceModules)")
-        message.append("\nproducts:\(context.package.products)")
-        message.append("\ntargets:\(context.package.targets)")
-        message.append("\narguments:\(arguments)")
+        var fileName =  sketchNames.isEmpty ? "NewCodeSketch" : sketchNames[0]
+        var fileContent = makeBasicCodeSketch(name: fileName)
+        fileName = fileName + ".swift"
         
+        // are not targets?
+        let targets = try? context.package.targets(named: targetNames)
+        if let targets {
+            for target in targets {
+                //guard let targetSource = target.sourceModule else { continue }
+                try writeToFile(location: target.directory.appending([fileName]), content: fileContent)
+            }
+        } else {
+            fileContent.append("\n//MARK: Command Arguments\n/*\(arguments)*/")
+            try writeToFile(location: context.package.directory.appending([fileName]), content: fileContent)
+            
+        }
         
-        var location = context.package.directory.appending([fileName])
-        //print(message, "\n")
-        let contributors = Set(message.components(separatedBy: CharacterSet.newlines)).sorted().filter { !$0.isEmpty }
-        try contributors.joined(separator: "\n").write(toFile: location.string, atomically: true, encoding: .utf8)
     }
     
+    func writeToFile(location:Path, content:some StringProtocol) throws {
+        try content.write(toFile: location.string, atomically: true, encoding: .utf8)
+    }
+    
+    
+    func makeBasicCodeSketch(name:String) -> String {
+        """
+public struct \(name):CodeSketch {
+    public typealias T = String
+
+    public init() {}
+    
+    func firstFunction() async throws -> String {
+        "up"
+    }
+
+    func secondFunction() async throws -> String {
+        "down"
+    }
+
+    func thirdFunction() async throws -> String {
+        "left"
+    }
+
+    func fourthFunction() async throws -> String {
+        "right"
+    }
+
+    public var itemsToRun: Dictionary<String, () async throws -> String> {
+        [
+            "a" : firstFunction,
+            "b" : secondFunction,
+            "c" : thirdFunction,
+            "d" : fourthFunction
+        ]
+    }
+}
+"""
+    }
     
 }
