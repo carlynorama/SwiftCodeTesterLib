4970ef2c252761b122dc2e9aef4f1a3f1ebe5208
carlynorama <carlynorama@gmail.com>
2024-01-14 09:34:06 -0800

    Add items for testing

--- Plugins/MakeCodeSketch/plugin.swift
+++ Plugins/MakeCodeSketch/plugin.swift
@@ -8,7 +8,15 @@ struct MakeCodeSketch: CommandPlugin {
         var message = "do I see this?\nhow about now?"
         let fileName = "codeSketch" + ".txt"
         //let location = context.pluginWorkDirectory // Is the BUILD folder
-        message.append("\n\(context)\n\(arguments)")
+        
+        message.append("\norigin: \(context.package.origin)")
+        message.append("\ndirectory: \(context.package.directory)")
+        message.append("\nsourceModules: \(context.package.sourceModules)")
+        message.append("\nproducts:\(context.package.products)")
+        message.append("\ntargets:\(context.package.targets)")
+        message.append("\narguments:\(arguments)")
+        
+        
         var location = context.package.directory.appending([fileName])
         //print(message, "\n")
         let contributors = Set(message.components(separatedBy: CharacterSet.newlines)).sorted().filter { !$0.isEmpty }
